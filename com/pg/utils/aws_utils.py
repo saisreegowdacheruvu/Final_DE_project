0b0bf4969efb945eb7f577e2690520adf2c27276
unknown <your Name>
2021-03-31 15:15:45 +0530

    modified

--- com/pg/utils/aws_utils.py
+++ com/pg/utils/aws_utils.py
@@ -46,7 +46,6 @@ def read_from_mongodb(spark, src_config):
     df_mongo = spark.read \
         .format("com.mongodb.spark.sql.DefaultSource") \
         .option("database", src_config["mongo_conf"]["database"]) \
-        .option("collection", src_config["mongo_conf"]["collection"]) \
         .load()
     return df_mongo
 

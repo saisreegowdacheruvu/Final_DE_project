8f9bcc38ddbc4ec3b12b98838eef3505cabdd570
unknown <your Name>
2021-03-31 19:55:54 +0530

    modified

--- com/pg/source_data_loading.py
+++ com/pg/source_data_loading.py
@@ -46,9 +46,7 @@ if __name__ == "__main__":
 
         elif src == "OL":
             # reading data from sftp server sftp_conf
-            path = src_config["sftp_conf"]["directory"] + "/" + src_config["file_name"]
-            print(path)
-            pem_file_path = os.path.abspath(current_dir + "/../../../../" + conf_secret_dir["sftp_conf"]["pem"])
+            pem_file_path = os.path.abspath(current_dir + "/../../../../../../" + conf_secret_dir["sftp_conf"]["pem"])
             print(pem_file_path)
             # file_name = ""
             df_sftp = ut.read_from_sftp(spark, conf_secret_dir, src_config, pem_file_path) \
